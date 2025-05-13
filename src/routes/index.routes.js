e76e208acfe1f72e4344def2969a73a22eac440f
Gabriela Fernanda <gabriela.f.barbosa@aluno.senai.br>
2025-05-13 11:51:38 -0300

    feat: creat record model, controllers and routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,6 +3,8 @@ import express from "express";
 // Importar todas as rotas
 import authRouter from "./auth.routes.js";
 import gameRouter from "./game.routes.js";
+import recordRouter from "./records.routes.js";
+
 
 import authMiddleware from "../middleware/authMiddleware.js";
 
@@ -11,6 +13,7 @@ const router = express.Router();
 // Rotas públicas
 router.use("/auth", authRouter);
 router.use("/games", gameRouter);
+router.use("/records", recordRouter);
 
 // Rotas protegidas
 router.use(authMiddleware);
